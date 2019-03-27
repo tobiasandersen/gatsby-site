b85667185af022241e2785e37506a0d74d82a132
Tobias Andersen <j.tobiasandersen@gmail.com>
2019-03-27 18:57:18 +0100

    add the problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+process.env.MY_VARIABLE = 'test'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
